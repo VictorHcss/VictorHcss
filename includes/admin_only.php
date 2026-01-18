f22140f2c727520e257ea931795e6f33354a9317
VictorHcss <henvictor2@gmail.com>
2026-01-18 17:20:24 -0300

    Erro README

--- includes/admin_only.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Requer que auth.php já tenha sido incluído ou a sessão iniciada
-if (!isset($_SESSION['role']) || $_SESSION['role'] !== 'admin') {
-    die("Acesso negado. Apenas administradores podem acessar esta página.");
-}
-?>
